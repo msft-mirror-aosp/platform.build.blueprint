9200994d37c130ce03db15fd227f182272c73e72
Colin Cross <ccross@android.com>
2025-04-14 15:29:02 -0700

    Free properties when freeing Module
    When FreeModuleAfterGenerateBuildActions is called blueprint frees
    the blueprint.Module object, but moduleInfo also has pointers to
    the property structs inside the blueprint.Module, preventing it
    from being freed.  The properties won't be used any more, so
    drop them too.
    
    Test: all Soong tests pass
    Change-Id: Iaede4d28ed2ba5af6c9896db2737fa63f5b7baae

--- context.go
+++ context.go
@@ -3517,6 +3517,8 @@ func (c *Context) generateModuleBuildActions(config interface{},
 				mctx.module.cachedName = mctx.module.logicModule.Name()
 				mctx.module.cachedString = mctx.module.logicModule.String()
 				mctx.module.logicModule = nil
+				mctx.module.properties = nil
+				mctx.module.propertyPos = nil
 			}
 
 			newErrs := c.processLocalBuildActions(&module.actionDefs,
