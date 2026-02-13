2d48f0af19d6d3ae028d714b99731f5d49c72559
Yu Liu <yudiliu@google.com>
2026-02-13 23:26:44 +0000

    Encode uniquelist as references.
    Bug: None
    Test: Local build.
    Change-Id: I01c44276adf8b9d9fe1647b2ded0e7892b78b4cc

--- gobtools/codegen/uniquelist.go
+++ gobtools/codegen/uniquelist.go
@@ -21,9 +21,29 @@ import (
 )
 
 func (g *gobGen) encodeUniqueList(encodeBody *strings.Builder, pkgName string, t *ast.IndexExpr, fieldName string) {
+	isZeroValue := g.nextVar()
+	typeRef := g.findTypeReference(t.Index, pkgName)
+	g.maybeAddImport(typeRef)
+
+	// Check if the list is empty/nil
+	encodeBody.WriteString(fmt.Sprintf("\t%s := %s.Len() == 0\n", isZeroValue, fieldName))
+	encodeBody.WriteString(fmt.Sprintf("\tif err = gobtools.EncodeBool(buf, %s); err != nil { return err }\n", isZeroValue))
+
+	encodeBody.WriteString(fmt.Sprintf("\tif !%s {\n", isZeroValue))
+	// Use EncodeReference to deduplicate the internal pointer of the UniqueList
+	encodeBody.WriteString(fmt.Sprintf("\t\tif err = gobtools.EncodeReference(ctx, %s, buf, func(v uniquelist.UniqueList[%s], buf *bytes.Buffer) error {\n", fieldName, typeRef.fullName()))
+
+	// Within the reference callback, encode the underlying slice
 	listName := g.nextVar()
-	encodeBody.WriteString(fmt.Sprintf("\t%s := %s.ToSlice()\n", listName, fieldName))
+	encodeBody.WriteString(fmt.Sprintf("\t\t\t%s := v.ToSlice()\n", listName))
 	g.encodeSlice(encodeBody, pkgName, t.Index, listName)
+
+	encodeBody.WriteString(fmt.Sprintf("\t\t\treturn nil\n"))
+	encodeBody.WriteString(fmt.Sprintf("\t\t}); err != nil { return err }\n"))
+	encodeBody.WriteString(fmt.Sprintf("\t}\n"))
+
+	g.imports[`"github.com/google/blueprint/uniquelist"`] = true
+	g.imports[`"github.com/google/blueprint/gobtools"`] = true
 }
 
 func (g *gobGen) hashUniqueList(hashBody *strings.Builder, pkgName string, t *ast.IndexExpr, fieldName string) {
@@ -39,11 +59,30 @@ func (g *gobGen) hashUniqueList(hashBody *strings.Builder, pkgName string, t *as
 }
 
 func (g *gobGen) decodeUniqueList(decodeBody *strings.Builder, pkgName string, t *ast.IndexExpr, fieldName string) {
-	listName := g.nextVar()
+	isZeroValue := g.nextVar()
+	decodeBody.WriteString(fmt.Sprintf("\tvar %s bool\n", isZeroValue))
+	decodeBody.WriteString(fmt.Sprintf("\tif err = gobtools.DecodeBool(buf, &%s); err != nil { return err }\n", isZeroValue))
+
+	decodeBody.WriteString(fmt.Sprintf("\tif !%s {\n", isZeroValue))
 	typeRef := g.findTypeReference(t.Index, pkgName)
-	decodeBody.WriteString(fmt.Sprintf("\tvar %s []%s\n", listName, typeRef.fullName()))
 	g.maybeAddImport(typeRef)
+
+	// Use DecodeReference to re-use existing UniqueList instances if they were already decoded
+	decodeBody.WriteString(fmt.Sprintf("\t\ttmp, err := gobtools.DecodeReference(ctx, &%s, buf, func(value *uniquelist.UniqueList[%s], buf *bytes.Reader) error {\n", fieldName, typeRef.fullName()))
+
+	listName := g.nextVar()
+	decodeBody.WriteString(fmt.Sprintf("\t\t\tvar %s []%s\n", listName, typeRef.fullName()))
 	g.decodeSlice(decodeBody, pkgName, t.Index, listName)
-	decodeBody.WriteString(fmt.Sprintf("\t%s = uniquelist.Make(%s)\n", fieldName, listName))
+
+	// Reify the slice into the canonical UniqueList
+	decodeBody.WriteString(fmt.Sprintf("\t\t\t*value = uniquelist.Make(%s)\n", listName))
+	decodeBody.WriteString(fmt.Sprintf("\t\t\treturn nil\n"))
+	decodeBody.WriteString(fmt.Sprintf("\t\t})\n"))
+
+	decodeBody.WriteString(fmt.Sprintf("\t\tif err != nil { return err }\n"))
+	decodeBody.WriteString(fmt.Sprintf("\t\t%s = *tmp\n", fieldName))
+	decodeBody.WriteString(fmt.Sprintf("\t}\n"))
+
 	g.imports[`"github.com/google/blueprint/uniquelist"`] = true
+	g.imports[`"github.com/google/blueprint/gobtools"`] = true
 }
