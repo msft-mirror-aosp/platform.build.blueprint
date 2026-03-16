ba3642b1f783b8fefcca52a7c230429b659e94b9
Pechetty Sravani (xWF) <pechetty@google.com>
2026-03-15 20:15:33 -0700

    Revert "Sort module variants before running singletons"
    This reverts commit 6ebd2523310f92e72fe8bb03186303a9e68bd1db.
    
    Reason for revert: <DroidMonitor: Potential culprit for http://b/492623434 - verifying through ABTD before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted.
    
    
    Fix: 492623434
    Change-Id: I13ff1de1d6cab35f7a0e2313b83d0058f75dc722

--- context.go
+++ context.go
@@ -3801,31 +3801,67 @@ func (c *Context) runMutator(config interface{}, mutatorGroup []*mutatorInfo,
 		}
 		module.group.modules = slices.Insert(module.group.modules, insertIndex, module)
 	}
-
-	for _, module := range onDemandModules {
-		insertIntoModuleGroup(module)
-		for _, fd := range module.newDirectDeps {
-			fd.reverseDeps = append(fd.reverseDeps, module)
-			// AddVariationDependency allows adding a dependency on itself, but only if
-			// that module is earlier in the module list than this one, since we always
-			// run GenerateBuildActions in order for the variants of a module
-			if fd.group == module.group && beforeInModuleList(module, fd, module.group.modules) {
-				return nil, []error{&BlueprintError{
-					Err: fmt.Errorf("%q depends on later version of itself", module.Name()),
-					Pos: module.pos,
-				}}
+	// Sort the on-demand variants before adding to moduleGroup.
+	// The variants will be ordered by SplitOnDemand values.
+	moduleLess := func(a, b *moduleInfo) int {
+		for _, mutator := range c.transitionMutatorNames {
+			aVariant := a.variant.variations.variations[mutator]
+			bVariant := b.variant.variations.variations[mutator]
+			if aVariant != bVariant {
+				aVariantIndex := -1
+				bVariantIndex := -1
+				for index, variant := range a.group.supportedVariantsOnDemand[mutator] {
+					if aVariant == variant.Variation() {
+						aVariantIndex = index
+					}
+					if bVariant == variant.Variation() {
+						bVariantIndex = index
+					}
+				}
+				if aVariantIndex != bVariantIndex {
+					return cmp.Compare(aVariantIndex, bVariantIndex)
+				}
+				return cmp.Compare(aVariant, bVariant)
 			}
 		}
+		return 0
+	}
+	// Group the on-demand variants per module group for faster sorting.
+	moduleGroupToOnDemandModules := make(map[*moduleGroup][]*moduleInfo)
+	for _, module := range onDemandModules {
+		moduleGroupToOnDemandModules[module.group] = append(moduleGroupToOnDemandModules[module.group], module)
+	}
+	for moduleGroup := range moduleGroupToOnDemandModules {
+		// TODO (b/448182248): Handle earlier variant creating transition mutators.
+		slices.SortFunc(moduleGroupToOnDemandModules[moduleGroup], moduleLess)
+	}
 
-		// The module has been created and mutated.
-		// For future mutators, this variant is the same as normal variants.
-		// Set this flag to false so that we do not try to call `rerunMutatorsOnVariantOnDemand` again.
-		module.createdOnDemand = false
-		module.createdOnDemandReplaceWith = nil
-		module.newDirectDeps = nil
-		module.newOnDemandReverseDeps = nil
-		module.createdOnDemandSupportedSplits = nil
-		module.group.cachedVariantsOnDemand = nil
+	for _, modules := range moduleGroupToOnDemandModules {
+		for _, module := range modules {
+			insertIntoModuleGroup(module)
+			for _, fd := range module.newDirectDeps {
+				fd.reverseDeps = append(fd.reverseDeps, module)
+				// AddVariationDependency allows adding a dependency on itself, but only if
+				// that module is earlier in the module list than this one, since we always
+				// run GenerateBuildActions in order for the variants of a module
+				if fd.group == module.group && beforeInModuleList(module, fd, module.group.modules) {
+					return nil, []error{&BlueprintError{
+						Err: fmt.Errorf("%q depends on later version of itself", module.Name()),
+						Pos: module.pos,
+					}}
+				}
+			}
+
+			// The module has been created and mutated.
+			// For future mutators, this variant is the same as normal variants.
+			// Set this flag to false so that we do not try to call `rerunMutatorsOnVariantOnDemand` again.
+			module.createdOnDemand = false
+			module.createdOnDemandReplaceWith = nil
+			module.newDirectDeps = nil
+			module.newOnDemandReverseDeps = nil
+			module.createdOnDemandSupportedSplits = nil
+			module.group.cachedVariantsOnDemand = nil
+		}
 	}
 
 	return deps, errs
@@ -4510,15 +4546,10 @@ func (c *Context) moduleGroupFromName(name string, namespace Namespace) *moduleG
 }
 
 func (c *Context) sortedModuleGroups() []*moduleGroup {
-	moduleLess := func(a, b *moduleInfo) int {
-		return cmp.Compare(a.variant.name, b.variant.name)
-	}
 	if c.cachedSortedModuleGroups == nil || c.cachedDepsModified {
 		unwrap := func(wrappers []ModuleGroup) []*moduleGroup {
 			result := make([]*moduleGroup, 0, len(wrappers))
 			for _, group := range wrappers {
-				// Order the module variants in-place
-				slices.SortFunc(group.moduleGroup.modules, moduleLess)
 				result = append(result, group.moduleGroup)
 			}
 			return result
